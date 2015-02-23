4956b4857b26d64517a37b2229cb2025a86f93aa
Dom4n <domanoid@gmail.com>
2015-02-23 18:06:10 +0100

    zaczyna to jakoś wyglądać, uporządkowane i działające (na razie bez sql)

--- accstats.py
+++ accstats.py
@@ -1,176 +1,169 @@
 __author__ = 'Doman'
 
 '''
-TEN SKRYPT ma za zadanie parsować plik logfile_console_XXXX.log,
+'PARSUJ' ma za zadanie parsować JEDEN plik logfile_console_XXXX.log,
 dane z niego wyciągnięte:
-nazwa_misji, mapa, długość_misji, ilość_graczy, lista_graczy
-dane te mają zostać zapisane w bazie danych lub json
+data, nazwa_misji, mapa, długość_misji, ilość_graczy, lista_graczy
+dane te mają zostać zapisane w bazie danych (lub json?)
 
-INNY SKRYPT(?) ma pozwolić na otworzenie bazy danych lub json
+'WYSWIETL' ma pozwolić na otworzenie bazy danych (lub json)
 i dowolne parsowanie danych po tych parametrach:
 - gracz -> w jakich dniach grał, zawężenie wyników
 - dzień/ilość graczy
 - dzień/długość misji
+- ...
 oraz wyświetlanie wyników na wykresach
 '''
 
-from pandas import *
-from datetime import *
-from dateutil.relativedelta import *
-import numpy as np
-import matplotlib.pyplot as plt
-import os, glob, time, datetime, fileinput, re, csv
+from pandas import *  # do gryzienia danych
+from dateutil.relativedelta import *  # dokładne przeliczenia na datach
+# import numpy as np
+# import matplotlib.pyplot as plt
+import os, glob, time, datetime, re
 
-class Parsuj(object):
-    # na stałe?
-    directory = 'F:/LOGS/test'
+
+class Parsuj:
+    def __init__(self):
+        directory = 'C:/Users/Jarek/PycharmProjects/ACC Stats'
+        self.changedir(directory)
+        file = self.findfile()
+        self.parserlogfile(file)
 
     # zmienia folder
-    # TODO: może argumentem to przesyłać? /kiedyś
+    # TODO: może ścieżka jako argument? /kiedyś
     def changedir(self, directory):
         os.chdir(directory)
         print(os.getcwd())
 
     # szuka zgodnego pliku
-    # misja musi się zacząć (do wybrania slotów) pomiędzy 20 a 21!
-    # TODO: zmieinć z czasu rozpoczęcia gry na czas załadowania misji!
+    # zazwyczaj plik ma datę o jeden dzień w przód!!
     def findfile(self):
         log = glob.glob('logfile_console*.log')
-        names = []
-        mtimes = []
-        for i in log:
-            # t = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(os.path.getmtime(i)))
-            t = time.gmtime(os.path.getmtime(i))
-            # if t.tm_hour >= 20:
-            if 1:
-                t = time.strftime('%Y-%m-%d %H:%M:%S', t)
-                # t = os.path.getmtime(i)
-                # print(os.path.getmtime(i))
-                names.append(i)
-                mtimes.append(t)
-
-        d = {'mtimes': mtimes,
-             'names': names}
-        dfilelist = DataFrame(d)
-        dfilelist = dfilelist.sort('mtimes')
-        return dfilelist
+        t = time.gmtime(os.path.getmtime(log[0]))
+        t2 = time.strftime('%Y-%m-%d', t)
+        plik = list([t2, log[0]])
+        return plik
 
     # kasuje powtarzające się nazwy
     def uniq(self, wejscie):
-        output = []
+        unikalne = []
         for x in wejscie:
-            if x not in output:
-                output.append(x)
-        return output
+            if x not in wejscie:
+                unikalne.append(x)
+        unikalne.sort()
+        return unikalne
 
     # parser danych z logfile
     # TODO: do generalnej przebudowy!!
-    def parserlogfile(self):
-        alldata = DataFrame(columns=['time', 'duration', 'misja', 'ilosc', 'gracze'])
-        onedata = DataFrame(columns=['time', 'duration', 'misja', 'ilosc', 'gracze'])
-        for file in dfilelist.values:
+    def parserlogfile(self, file):
+        # data, nazwa_misji, mapa, długość_misji, ilość_graczy, lista_graczy
+        dane = DataFrame(columns=['data', 'nazwa_misji', 'mapa', 'dlugosc_misji',
+                                  'ilosc_graczy', 'lista_graczy'])
+        plik = open(file[1], "r")
+        lista_graczy = []
+        ilosc = 0
+        dlugosc_misji = 0
+        mapa = ''
+        data = file[0]
+        nazwa_misji = ''
 
-            file = file[1]
-            logg = open(file, "r")
-            gracze = []
-            ilosc = 0
+        for line in plik:
 
-            for line in logg:
+            if 'Mission read' in line:
+                endtime = line[:8]
+                if not line.startswith('20'):
+                    break
 
-                if re.match('(.*)(Game started|Game restarted)(.*)', line):
-                    t1 = re.search(
-                        '((?:(?:[0-1][0-9])|(?:[2][0-3])|(?:[0-9])):(?:[0-5][0-9])(?::[0-5][0-9])?(?:\\s?(?:am|AM|pm|PM))?)',
-                        line).group(1)
-                    t2 = re.search("^2[0-3]", t1)
-                    try:
-                        t2 = t2.group(0)
-                    except:
-                        break
+            if 'mission=' in line:
+                nazwa_misji = line.split('"')[1]
 
-                if re.match('(.*)mission=(.*)', line):
-                    misja = re.search('.*?(".*?")', line).group(1).replace('\"', '')
+            if 'island=' in line:
+                mapa = line.split('"')[1]
 
-                if re.match('(.*)duration=(.*)', line):
-                    duration = re.search("\d+", line).group(0)
-                    duration = round((int(duration) / 60), 1)
+            if 'duration=' in line:
+                dlugosc_misji = float(line.split('=')[1][:5])
 
-                if re.match('(.*)name=(.*)', line):
-                    gracz = re.search('.*?(".*?")', line).group(1)
-                    gracz = gracz.replace('\"', '')
-                    gracze.append(gracz)
+            if 'name=' in line:
+                gracz = line.split('"')[1]
+                lista_graczy.append(gracz)
 
-                    ######
-                    czasepoch = os.path.getmtime(file)
-                    czasepoch = datetime.datetime.fromtimestamp(czasepoch)
-                    t3 = time.strptime(t1, '%H:%M:%S')
-                    if (t3.tm_hour + (duration / 60) >= 24) or (t3.tm_hour < 20):
-                        # print(czasepoch)
-                        czasepoch = czasepoch + relativedelta(days=-1)
-                    czas = time.strftime('%Y %m %d', czasepoch.timetuple())
-                #####
-                try:
-                    ilosc = len(gracze)
-                    d0 = {'time': czas,
-                          'duration': duration,
-                          'misja': misja,
-                          'ilosc': ilosc,
-                          'gracze': [gracze]}
-                    onedata = onedata.append(
-                        DataFrame(d0, columns=['time', 'duration', 'misja', 'ilosc', 'gracze'], index=[misja]))
-                    # print(onedata)
-                except:
-                    pass
+            if ('Game restarted' in line) or ('Game finished' in line):
+                endtime = line.split(' ')[0][:8]
+                if endtime == '':
+                    endtime = line.split(' ')[1][:8]
+                endtime = time.strptime(endtime, '%H:%M:%S')
+                if dlugosc_misji > 0:
+                    if (endtime.tm_hour + (dlugosc_misji / 3600) >= 24) or (endtime.tm_hour < 20):
+                        data = data + relativedelta(days=-1)
+                        data = time.strftime('%Y %m %d', data.timetuple())
 
-                if re.match('(.*)Waiting for next game(.*)', line):
-                    gracze = []
-                    # print(onedata)
-                    # break
+        ilosc = len(lista_graczy)
+        global d0
+        d0 = {'data': data,
+              'nazwa_misji': nazwa_misji,
+              'mapa': mapa,
+              'dlugosc_misji': dlugosc_misji,
+              'ilosc_graczy': ilosc,
+              'lista_graczy': [lista_graczy]}
+        # dane = dane.
+        # onedata = onedata.append(
+        #     DataFrame(d0, columns=['time', 'duration', 'misja', 'ilosc', 'gracze'], index=[misja]))
+        # print(onedata)
+        dane = DataFrame(d0, columns=['data',
+                                      'nazwa_misji',
+                                      'mapa',
+                                      'dlugosc_misji',
+                                      'ilosc_graczy',
+                                      'lista_graczy'])
 
-            if (ilosc > 4 ) and (duration > 1):
-                # czas = time.gmtime(os.path.getmtime(file))
-                # if (czasepoch.tm_hour+duration >= 24):
-                #    czasepoch.tm_mday = czasepoch.tm_mday - 1
+        print(d0)
+        print(dane)
 
-                # gracze = uniq(gracze)
-                # ilosc = len(gracze)
+        # if (ilosc > 4 ) and (duration > 1):
+            # czas = time.gmtime(os.path.getmtime(file))
+            # if (czasepoch.tm_hour+duration >= 24):
+            #    czasepoch.tm_mday = czasepoch.tm_mday - 1
 
-                # d1 = {'time':czas,
-                #     'duration':duration,
-                #   'misja':misja,
-                #  'ilosc':ilosc,
-                # 'gracze':[gracze]}
+            # gracze = uniq(gracze)
+            # ilosc = len(gracze)
 
-                # alldata = alldata.append(DataFrame(d1, columns=['time','duration','misja','ilosc','gracze']))
-                maks = onedata['duration'].idxmax()
-                print(maks)
-                # onedata.loc[maks]
-                alldata = alldata.append(onedata.loc[maks])
+            # d1 = {'time':czas,
+            #     'duration':duration,
+            #   'misja':misja,
+            #  'ilosc':ilosc,
+            # 'gracze':[gracze]}
 
-        maks = onedata['duration'].idxmax()
-        print(maks)
-        onedata.loc[maks]
+            # alldata = alldata.append(DataFrame(d1, columns=['time','duration','misja','ilosc','gracze']))
+            # maks = onedata['duration'].idxmax()
+            # print(maks)
+            # onedata.loc[maks]
+            # alldata = alldata.append(onedata.loc[maks])
 
-    # Jakieś gówno
-    def gowno(self):
-        czasepoch = os.path.getmtime(file)
-        czase2 = datetime.datetime.fromtimestamp(czasepoch)
-        print(czase2)
-        czase2 = czase2 + relativedelta(minutes=-duration)
-        print(czase2)
-        czase2
+        # maks = onedata['duration'].idxmax()
+        # print(maks)
+        # onedata.loc[maks]
 
-        print(t1)
-        t3 = time.strptime(t1, '%H:%M:%S')
-        t3
-
-        czase2 - relativedelta(days=1)
+    # # Jakieś gówno
+    # def gowno(self):
+    #     czasepoch = os.path.getmtime(file)
+    #     czase2 = datetime.datetime.fromtimestamp(czasepoch)
+    #     print(czase2)
+    #     czase2 = czase2 + relativedelta(minutes=-duration)
+    #     print(czase2)
+    #     czase2
+    #
+    #     print(t1)
+    #     t3 = time.strptime(t1, '%H:%M:%S')
+    #     t3
+    #
+    #     czase2 - relativedelta(days=1)
 
 
-class Wyswietl(object):
-    pass
+#class Wyswietl(object):
+#    pass
 
 # TODO: jednak wszystko w jednym pliku, ta klasa parsuje plik logfile
-Parsuj()
+# Parsuj()
 
 # TODO: ta klasa bedzie zawierala wyswietlanie wynikow wedlug kryteriow
-Wyswietl()
+# Wyswietl()
